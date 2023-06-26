e4d57c1bcc360c9948a019c9b97dbdd07e16a057
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-06-26 17:19:06 -0400

    python

--- intro.py
+++ intro.py
@@ -1,6 +1,7 @@
-""""system.module."""
-
-print("Hola mundo ")
-print("la mosca"*4)
 
 
+descripcion = """"
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus earum omnis minima eligendi dolor facere quod reiciendis in deserunt nulla vero blanditiis ipsam, hic et eveniet laboriosam! Laudantium, natus consequatur.
+"""
+
+print(descripcion)
