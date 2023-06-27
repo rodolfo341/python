a79652477274ae7276199e55fcf4dde6e7664722
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-06-26 20:44:43 -0400

    python

--- intro.py
+++ intro.py
@@ -1,6 +1,17 @@
 
 
-nombre = "Rodolfo"
-apellido = "Aravena"
-print(f"{nombre} {apellido}")
-print(f"{nombre[0]} {2 + 5}")
+nombre = "rodolFo"
+print("upper " + nombre.upper())
+print("lower " + nombre.lower())
+print("capitalize " + nombre.capitalize())
+print("strip " + nombre.strip())
+print("strip.capitalize " + nombre.strip().capitalize())
+print("title " + nombre.title())
+print("lstrip " + nombre.lstrip())
+print("rstrip " + nombre.rstrip())
+print(nombre.find("do"))
+print("replace(o por a) " + nombre.replace("o", "a"))
+print("do" in nombre)
+print("do" not in nombre)
+
+
