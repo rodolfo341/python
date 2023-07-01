57ceadf072e2b1ba371333718911f3496d0d5408
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-06-30 22:32:08 -0400

    python

--- intro.py
+++ intro.py
@@ -1,4 +1,12 @@
+#import math
+continuar = True
 
+while continuar:
+    edad = int(input("edad: "))
+    res = "puede entrar" if 50 >= edad >=15 else "no puede entrar"
+    print(res)
 
+    respuesta = input("¿Deseas continuar? (s/n): ")
+    print("Continuando..." if respuesta.lower() == "s" else "Saliendo...")
+    continuar = respuesta.lower() == "s"
 
-print(round(1.3))
