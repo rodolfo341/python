e98c5f2e851ba997e12a47392b9d09ce9ea014e1
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-06-30 21:09:13 -0400

    python

--- intro.py
+++ intro.py
@@ -1,24 +1,4 @@
 
-print("Calculadora")
 
-num1 = int(input("Ingrese numero 1: "))
-num2 = int(input("ingrese numero 2: "))
-operador = input("ingrese operador ( +, -, *, / )")
-resultado = 0
-Flag = True
-if operador=="+":
-    resultado=num1+num2
-elif operador=="-":
-    resultado=num1-num2
-elif operador=="*":
-    resultado=num1*num2
-elif operador=="/":
-    resultado=num1/num2
-else:
-    flag=False
-if flag:
-    print(num1 , operador, num2, " = ", resultado)
-else:
-    print("opcion errada")
-    
-    
+
+print(round(1.3))
