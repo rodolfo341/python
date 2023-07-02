73b38c20585a4e4e2d24aad8aec0b0d124089d3e
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-07-01 20:27:10 -0400

    python

--- intro.py
+++ intro.py
@@ -3,10 +3,15 @@ continuar = True
 
 while continuar:
     
-    for numero in range(20):
-        print(numero)
+    el_numero = 3
+    for iterador in range(20):
+       print(iterador)
+       if iterador == 7:
+          print("encontrado")
+    
 
     respuesta = input("¿Deseas continuar? (s/n): ")
     print("Continuando..." if respuesta.lower() == "s" else "Saliendo...")
     continuar = respuesta.lower() == "s"
 
+ 
