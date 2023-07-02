e46810cb7532f96d1cb63417330bd270e245099a
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-07-02 18:43:04 -0400

    python

---
+++ python30dias/dia-1.py
@@ -0,0 +1,36 @@
+
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+# print(2 + 3)   # addition(+)
+# print(3 - 1)   # subtraction(-)
+# print(2 * 3)   # multiplication(*)
+# print(3 / 2)   # division(/)
+# print(3 ** 2)  # exponential(**)
+# print(3 % 2)   # modulus(%)
+# print(10 // 3)  # Floor division operator(//)
+
+# # Checking data types
+
+# print(type(10))                  # Int
+# print(type(3.14))                # Float
+# print(type(1 + 3j))              # Complex
+# print(type('Asabeneh'))          # String
+# print(type([1, 2, 3]))           # List
+# print(type({'name':'Asabeneh'})) # Dictionary
+# print(type({9.8, 3.14, 2.7}))    # Set
+# print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+
+
+
+
+# continuar = True
+# while continuar:
+
+    
+
+#     respuesta = input("¿Deseas continuar? (s/n): ")
+#     print("Continuando..." if respuesta.lower() == "s" else "Saliendo...")
+#     continuar = respuesta.lower() == "s"
