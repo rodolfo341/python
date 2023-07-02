5af6230576b543e16185d471620ddfddbc41041e
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-07-02 19:19:49 -0400

    python

---
+++ python30dias/dia-2.py
@@ -0,0 +1,49 @@
+import msvcrt
+def esperar_tecla_para_continuar():
+    print("Presiona cualquier tecla para continuar...")
+    msvcrt.getch()
+
+print("declaro las variables")
+esperar_tecla_para_continuar()
+
+nombre = 'Rodolfo'
+apellido = 'aravena'
+pais = 'Chile'
+ciudad = 'Santiago'
+edad = 51
+esta_casado = True
+habilidades = ['HTML', 'CSS', 'JS', 'React', 'Python']
+informacion_personal = {
+    'nombre':'Rodolfo', 
+    'apellido':'aravena', 
+    'pais':'Chile',
+    'ciudad':'Santiago'
+    }
+
+# Imprimir los valores almacenados en las variables
+print("Imprimo las variables")
+esperar_tecla_para_continuar()
+
+print('Nombre:', nombre)
+print('Nombre longitud(length):', len(nombre))
+print('Apellido: ', apellido)
+print('Apellido longitud(length): ', len(apellido))
+print('Pais: ', pais)
+print('Ciudad: ', ciudad)
+print('Edad: ', edad)
+print('Casado: ', esta_casado)
+print('Habilidades: ', habilidades)
+print('Informacion personalion: ', informacion_personal)
+
+# Declarar múltiples variables en una línea
+print("Declaro e imprimo variables multiples")
+esperar_tecla_para_continuar()
+
+nombre, apellido, pais, edad, esta_casado = 'Rodolfo', 'Aravena', 'Chile', 51, True
+
+print(nombre, apellido, pais, edad, esta_casado)
+print('Nombre:', nombre)
+print('Apellido: ', apellido)
+print('Pais: ', pais)
+print('Edad: ', edad)
+print('Esta casado: ', esta_casado)
