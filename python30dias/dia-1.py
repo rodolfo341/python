4852a6ff0cc215654135bd462c6db72ff9df72b8
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-07-04 19:01:53 -0400

    python desafio 30 dias dia 2

--- python30dias/dia-1.py
+++ python30dias/dia-1.py
@@ -4,6 +4,9 @@ import msvcrt
 def esperar_tecla_para_continuar():
     print("Presiona cualquier tecla para continuar...")
     msvcrt.getch()
+    def esperar_tecla_para_continuar():
+        print("Presiona cualquier tecla para continuar...")
+        msvcrt.getch()
 
 # Introducción
 # Dia 1 de 30 Dias del desafio Python
