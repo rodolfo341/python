79cc44de8ed1ffc770c22a0357f7760eb79c5281
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-07-14 17:24:41 -0400

    python desafio 30 dias dia 3

--- python30dias/dia-3/dia-3-ejercicios.py
+++ python30dias/dia-3/dia-3-ejercicios.py
@@ -4,20 +4,20 @@ def esperar_tecla_para_continuar():
     msvcrt.getch()
     
 
-# 1   Declara tu edad como variable entera
+# 1   Declara tu edad como variable Ingresea
 # 2   Declara tu altura como una variable flotante
 # 3   Declarar una variable que almacene un número complejo
 # 4   Escriba un script que solicite al usuario que ingrese la base y la altura del triángulo y 
 #     calcule el área de este triángulo (área = 0,5 xbxh).
-#           Enter base: 20
-#           Enter height: 10
-#           The area of the triangle is 100
+#           Ingrese base: 20
+#           Ingrese altura: 10
+#           el area del el triangulo is 100
 # 5   Escriba un script que solicite al usuario que ingrese el lado a, el lado b y el lado c del triángulo. 
 #     Calcula el perímetro del triángulo (perímetro = a + b + c). 
-#           Enter side a: 5
-#           Enter side b: 4
-#           Enter side c: 3
-#           The perimeter of the triangle is 12
+#           Ingrese lado a: 5
+#           Ingrese lado b: 4
+#           Ingrese lado c: 3
+#           el perimeter del el triangulo is 12
 # 6   Obtenga la longitud y el ancho de un rectángulo usando el indicador. 
 #     Calcula su área (área = largo x ancho) y perímetro (perímetro = 2 x (largo + ancho))
 # 7   Obtenga el radio de un círculo usando el indicador. 
@@ -38,13 +38,13 @@ def esperar_tecla_para_continuar():
 # 20  Comprueba si int('9.8') es igual a 10
 # 21  Escriba un script que solicite al usuario que ingrese las horas y la tarifa por hora. 
 #     ¿Calcular el salario de la persona?
-#               Enter hours: 40
-#               Enter rate per hour: 28
+#               Ingrese hours: 40
+#               Ingrese rate per hour: 28
 #               Your weekly earning is 1120
 # 22  Escriba un script que solicite al usuario que ingrese el número de años. 
 #     Calcular el número de segundos que puede vivir una persona. 
 #     Supongamos que una persona puede vivir cien años. 
-#               Enter number of years you have lived: 100
+#               Ingrese number del years you have lived: 100
 #               You have lived for 3153600000 seconds.
 # 23  Escriba un script de Python que muestre la siguiente tabla 
 #               1 1 1 1 1
@@ -54,94 +54,141 @@ def esperar_tecla_para_continuar():
 #               5 1 5 25 125
 
 
-# 1   Declara tu edad como variable entera
+# 1   Declara tu edad como variable Ingresea
 edad = 51
-print(f"moi edad es: {edad}")
+print(f"Mi edad es: {edad}")
 esperar_tecla_para_continuar()
 
 # 2   Declara tu altura como una variable flotante
 altura = 1.7
-print(f"mi estatura es{altura}")
+print(f"mi estatura es {altura}")
 esperar_tecla_para_continuar()
 
 # 3   Declarar una variable que almacene un número complejo
-
+num_complejo = (1 + 1j)
+print(f"El valor del numero complejo es: {num_complejo}")
+esperar_tecla_para_continuar()
 
 # 4   Escriba un script que solicite al usuario que ingrese la base y la altura del triángulo y 
-#     calcule el área de este triángulo (área = 0,5 xbxh).
-#           Enter base: 20
-#           Enter height: 10
-#           The area of the triangle is 100
+#     calcule el área de este triángulo (área = 0,5 x b x h).
+#           Ingrese base: 20
+#           Ingrese altura: 10
+#           El area del triangulo sera 100
+base_triangulo = float(input("Ingrese base: "))
+altura_triangulo = float(input("Ingrese altura: "))
+area = 0.5 * base_triangulo * altura_triangulo
+print(f"El area del triangulo es: {area}")
+esperar_tecla_para_continuar()
+
+
 # 5   Escriba un script que solicite al usuario que ingrese el lado a, el lado b y el lado c del triángulo. 
 #     Calcula el perímetro del triángulo (perímetro = a + b + c). 
-#           Enter side a: 5
-#           Enter side b: 4
-#           Enter side c: 3
-#           The perimeter of the triangle is 12
-
+#           Ingrese lado a: 5
+#           Ingrese lado b: 4
+#           Ingrese lado c: 3
+#           el perimetro del triangulo is 12
+lado_a = int(input("ingrese lado a"))
+lado_b = int(input("ingrese lado b"))
+lado_c = int(input("ingrese lado c"))
+perimetro = lado_a + lado_b + lado_c
+print(f"El area del triangulo es: {area}")
+esperar_tecla_para_continuar()
 
 # 6   Obtenga la longitud y el ancho de un rectángulo usando el indicador. 
 #     Calcula su área (área = largo x ancho) y perímetro (perímetro = 2 x (largo + ancho))
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 7   Obtenga el radio de un círculo usando el indicador. 
 #     Calcula el área (área = pi xrxr) y la circunferencia (c = 2 x pi xr) donde pi = 3,14.
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 8   Calcule la pendiente, la intersección x y la intersección y de y = 2x -2
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 9   La pendiente es (m = y2-y1/x2-x1). Encuentre la pendiente y la distancia euclidiana entre el punto 
 #     (2, 2) y el punto (6,10)
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 10  Compara las pendientes en las tareas 8 y 9.
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 11  Calcula el valor de y (y = x^2 + 6x + 9). Trate de usar diferentes valores de x y descubra en qué valor de x y será 0
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 12  Encuentre la longitud de 'python' y 'dragon' y haga una declaración de comparación falsa.
 
-
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 13  Use un operador para verificar si 'on' se encuentra tanto en 'python' como en 'dragon'
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 14  Espero que este curso no esté lleno de jerga . Use el operador in para verificar si hay jerga en la oración.
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 15  No hay 'on' tanto en dragon como en python
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 16  Encuentre la longitud del texto python y convierta el valor en flotante y conviértalo en una cadena
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 17  Los números pares son divisibles por 2 y el resto es cero. ¿Cómo verifica si un número es par o no usando python?
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 18  Verifique si la división del piso de 7 por 3 es igual al valor int convertido de 2.7.
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 19  Compruebe si el tipo de '10' es igual al tipo de 10
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 20  Comprueba si int('9.8') es igual a 10
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 21  Escriba un script que solicite al usuario que ingrese las horas y la tarifa por hora. 
 #     ¿Calcular el salario de la persona?
-#               Enter hours: 40
-#               Enter rate per hour: 28
+#               Ingrese hours: 40
+#               Ingrese rate per hour: 28
 #               Your weekly earning is 1120
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 22  Escriba un script que solicite al usuario que ingrese el número de años. 
 #     Calcular el número de segundos que puede vivir una persona. 
 #     Supongamos que una persona puede vivir cien años. 
-#               Enter number of years you have lived: 100
+#               Ingrese number del years you have lived: 100
 #               You have lived for 3153600000 seconds.
 
+print(f": {x}")
+esperar_tecla_para_continuar()
 
 # 23  Escriba un script de Python que muestre la siguiente tabla 
 #               1 1 1 1 1
@@ -150,4 +197,5 @@ esperar_tecla_para_continuar()
 #               4 1 4 16 64
 #               5 1 5 25 125
 
-
+print(f": {x}")
+esperar_tecla_para_continuar()
