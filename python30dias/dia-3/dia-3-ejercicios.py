920160a6cf0c4d15d831100494ae112f50319795
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-07-14 18:54:44 -0400

    python desafio 30 dias dia 3

--- python30dias/dia-3/dia-3-ejercicios.py
+++ python30dias/dia-3/dia-3-ejercicios.py
@@ -1,3 +1,4 @@
+import math
 import msvcrt
 def esperar_tecla_para_continuar():
     print("\n\tPresiona cualquier tecla para continuar... \n")
@@ -87,90 +88,109 @@ esperar_tecla_para_continuar()
 #           Ingrese lado b: 4
 #           Ingrese lado c: 3
 #           el perimetro del triangulo is 12
-lado_a = int(input("ingrese lado a"))
-lado_b = int(input("ingrese lado b"))
-lado_c = int(input("ingrese lado c"))
+lado_a = int(input("ingrese lado a: "))
+lado_b = int(input("ingrese lado b: "))
+lado_c = int(input("ingrese lado c: "))
 perimetro = lado_a + lado_b + lado_c
-print(f"El area del triangulo es: {area}")
+print(f"El area del triangulo es: {perimetro}")
 esperar_tecla_para_continuar()
 
 # 6   Obtenga la longitud y el ancho de un rectángulo usando el indicador. 
 #     Calcula su área (área = largo x ancho) y perímetro (perímetro = 2 x (largo + ancho))
-
-print(f": {x}")
+alto = int(input("Ingrese alto: "))
+ancho = int(input("Ingrese ancho: "))
+area = alto * ancho
+perimetro = 2 * (alto + ancho)
+print(f"El area del rectangulo es: {area}")
+print(f"El perimetro del rectangulo: {perimetro}")
 esperar_tecla_para_continuar()
 
 # 7   Obtenga el radio de un círculo usando el indicador. 
-#     Calcula el área (área = pi xrxr) y la circunferencia (c = 2 x pi xr) donde pi = 3,14.
-
-print(f": {x}")
+#     Calcula el área (área = pi x r x r) y la circunferencia (c = 2 x pi x r) donde pi = 3,14.
+radio = float(input("Radio: "))
+area = round(math.pi * radio ** 2,2)
+circunferencia = round(2 * math.pi * radio,2)
+print(f"Area: {area}")
+print(f"Circunferencia: {circunferencia}")
 esperar_tecla_para_continuar()
 
 # 8   Calcule la pendiente, la intersección x y la intersección y de y = 2x -2
+pendiente8 = 2
+interseccion_y = -2
+interseccion_x = (0 - interseccion_y) / pendiente8
 
-print(f": {x}")
+print(f"La pendiente es: {pendiente8}")
+print(f"La intersección y es: {interseccion_y}")
+print(f"La intersección x es: {interseccion_x}")
 esperar_tecla_para_continuar()
 
 # 9   La pendiente es (m = y2-y1/x2-x1). Encuentre la pendiente y la distancia euclidiana entre el punto 
 #     (2, 2) y el punto (6,10)
+x1 = 2
+y1 = 2
+x2 = 6
+y2 = 10
+pendiente9 = (y2 - y1) / (x2 - x1)
+distancia = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
-print(f": {x}")
+print(f"La pendiente es: {pendiente9}")
+print(f"La distancia euclidiana es: {distancia}")
 esperar_tecla_para_continuar()
 
 # 10  Compara las pendientes en las tareas 8 y 9.
-
-print(f": {x}")
+print(f"Pendiente8: {pendiente8}")
+print(f"Pendiente9: {pendiente9}")
 esperar_tecla_para_continuar()
 
 # 11  Calcula el valor de y (y = x^2 + 6x + 9). Trate de usar diferentes valores de x y descubra en qué valor de x y será 0
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 12  Encuentre la longitud de 'python' y 'dragon' y haga una declaración de comparación falsa.
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 13  Use un operador para verificar si 'on' se encuentra tanto en 'python' como en 'dragon'
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 14  Espero que este curso no esté lleno de jerga . Use el operador in para verificar si hay jerga en la oración.
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 15  No hay 'on' tanto en dragon como en python
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 16  Encuentre la longitud del texto python y convierta el valor en flotante y conviértalo en una cadena
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 17  Los números pares son divisibles por 2 y el resto es cero. ¿Cómo verifica si un número es par o no usando python?
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 18  Verifique si la división del piso de 7 por 3 es igual al valor int convertido de 2.7.
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 19  Compruebe si el tipo de '10' es igual al tipo de 10
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 20  Comprueba si int('9.8') es igual a 10
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 21  Escriba un script que solicite al usuario que ingrese las horas y la tarifa por hora. 
 #     ¿Calcular el salario de la persona?
@@ -178,8 +198,8 @@ esperar_tecla_para_continuar()
 #               Ingrese rate per hour: 28
 #               Your weekly earning is 1120
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 22  Escriba un script que solicite al usuario que ingrese el número de años. 
 #     Calcular el número de segundos que puede vivir una persona. 
@@ -187,8 +207,8 @@ esperar_tecla_para_continuar()
 #               Ingrese number del years you have lived: 100
 #               You have lived for 3153600000 seconds.
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
 
 # 23  Escriba un script de Python que muestre la siguiente tabla 
 #               1 1 1 1 1
@@ -197,5 +217,5 @@ esperar_tecla_para_continuar()
 #               4 1 4 16 64
 #               5 1 5 25 125
 
-print(f": {x}")
-esperar_tecla_para_continuar()
+#print(f": {}")
+#esperar_tecla_para_continuar()
