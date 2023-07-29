24b48bc378ed8fdf927868f5e598792660be4b73
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-07-29 10:09:35 -0400

    python desafio 30 dias dia 5

--- python30dias/dia-5/dia-5.py
+++ python30dias/dia-5/dia-5.py
@@ -4,18 +4,18 @@ def esperar_tecla_para_continuar():
     msvcrt.getch()
 
 
-empty_list = list() # this is an empty list, no item in the list
-print(len(empty_list)) # 0
+lista_vacia = list() # this is an empty list, no item in the list
+print(len(lista_vacia)) # 0
 
-fruits = ['banana', 'orange', 'mango', 'lemon']                     # list of fruits
-vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']      # list of vegetables
-animal_products = ['milk', 'meat', 'butter', 'yoghurt']             # list of animal products
+frutas = ['platano', 'naranja', 'durazno', 'limon']                     # list of frutas
+vegetables = ['Tomates', 'Papas', 'Repollo','Cebolla', 'Zanahoria']      # list of vegetables
+animal_products = ['leche', 'carne', 'mantequilla', 'yoghurt']             # list of animal products
 web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] # list of web technologies
-countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+countries = ['Finlandia', 'Estonia', 'Denmark', 'Sweden', 'Norway']
 
 # Print the lists and it length
-print('Fruits:', fruits)
-print('Number of fruits:', len(fruits))
+print('frutas:', frutas)
+print('Number of frutas:', len(frutas))
 print('Vegetables:', vegetables)
 print('Number of vegetables:', len(vegetables))
 print('Animal products:',animal_products)
@@ -26,104 +26,104 @@ print('Number of countries:', len(countries))
 
 # Modifying list
 
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-first_fruit = fruits[0] # we are accessing the first item using its index
-print(first_fruit)      # banana
-second_fruit = fruits[1]
-print(second_fruit)     # orange
-last_fruit = fruits[3]
-print(last_fruit) # lemon
+frutas = ['platano', 'naranja', 'durazno', 'limon'] 
+first_fruit = frutas[0] # we are accessing the first item using its index
+print(first_fruit)      # platano
+second_fruit = frutas[1]
+print(second_fruit)     # naranja
+last_fruit = frutas[3]
+print(last_fruit) # limon
 # Last index
-last_index = len(fruits) - 1
-last_fruit = fruits[last_index]
+last_index = len(frutas) - 1
+last_fruit = frutas[last_index]
 
 # Accessing items
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-last_fruit = fruits[-1]
-second_last = fruits[-2]
-print(last_fruit)       # lemon
-print(second_last)      # mango
+frutas = ['platano', 'naranja', 'durazno', 'limon'] 
+last_fruit = frutas[-1]
+second_last = frutas[-2]
+print(last_fruit)       # limon
+print(second_last)      # durazno
 
 # Slicing items
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-all_fruits = fruits[0:4] # it returns all the fruits
+frutas = ['platano', 'naranja', 'durazno', 'limon'] 
+all_frutas = frutas[0:4] # it returns all the frutas
 # this is also give the same result as the above
-all_fruits = fruits[0:] # if we don't set where to stop it takes all the rest
-orange_and_mango = fruits[1:3] # it does not include the end index
-orange_mango_lemon = fruits[1:]
+all_frutas = frutas[0:] # if we don't set where to stop it takes all the rest
+naranja_and_durazno = frutas[1:3] # it does not include the end index
+naranja_durazno_limon = frutas[1:]
 
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-all_fruits = fruits[-4:] # it returns all the fruits
+frutas = ['platano', 'naranja', 'durazno', 'limon'] 
+all_frutas = frutas[-4:] # it returns all the frutas
 # this is also give the same result as the above
-orange_and_mango = fruits[-3:-1] # it does not include the end index
-orange_mango_lemon = fruits[-3:]
+naranja_and_durazno = frutas[-3:-1] # it does not include the end index
+naranja_durazno_limon = frutas[-3:]
 
 
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-fruits[0] = 'Avocado' 
-print(fruits)       #  ['avocado', 'orange', 'mango', 'lemon']
-fruits[1] = 'apple'
-print(fruits)       #  ['avocado', 'apple', 'mango', 'lemon']
-last_index = len(fruits) - 1
-fruits[last_index] = 'lime'
-print(fruits)        #  ['avocado', 'apple', 'mango', 'lime']
+frutas = ['platano', 'naranja', 'durazno', 'limon'] 
+frutas[0] = 'Avocado' 
+print(frutas)       #  ['avocado', 'naranja', 'durazno', 'limon']
+frutas[1] = 'apple'
+print(frutas)       #  ['avocado', 'apple', 'durazno', 'limon']
+last_index = len(frutas) - 1
+frutas[last_index] = 'lime'
+print(frutas)        #  ['avocado', 'apple', 'durazno', 'lime']
 
 # checking items
-fruits = ['banana', 'orange', 'mango', 'lemon']
-does_exist = 'banana' in fruits
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+does_exist = 'platano' in frutas
 print(does_exist)  # True
-does_exist = 'lime' in fruits
+does_exist = 'lime' in frutas
 print(does_exist)  # False
 
 # Append
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.append('apple')
-print(fruits)           # ['banana', 'orange', 'mango', 'lemon', 'apple']
-fruits.append('lime')   # ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime]
-print(fruits)
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+frutas.append('apple')
+print(frutas)           # ['platano', 'naranja', 'durazno', 'limon', 'apple']
+frutas.append('lime')   # ['platano', 'naranja', 'durazno', 'limon', 'apple', 'lime]
+print(frutas)
 
 # insert
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.insert(2, 'apple') # insert apple between orange and mango
-print(fruits)           # ['banana', 'orange', 'apple', 'mango', 'lemon']
-fruits.insert(3, 'lime')   # ['banana', 'orange', 'apple', 'mango', 'lime','lemon',]
-print(fruits)
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+frutas.insert(2, 'apple') # insert apple between naranja and durazno
+print(frutas)           # ['platano', 'naranja', 'apple', 'durazno', 'limon']
+frutas.insert(3, 'lime')   # ['platano', 'naranja', 'apple', 'durazno', 'lime','limon',]
+print(frutas)
 
 # remove
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.remove('banana')
-print(fruits)  # ['orange', 'mango', 'lemon']
-fruits.remove('lemon')
-print(fruits)  # ['orange', 'mango']
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+frutas.remove('platano')
+print(frutas)  # ['naranja', 'durazno', 'limon']
+frutas.remove('limon')
+print(frutas)  # ['naranja', 'durazno']
 
 # pop
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.pop()     
-print(fruits)       # ['banana', 'orange', 'mango']
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+frutas.pop()     
+print(frutas)       # ['platano', 'naranja', 'durazno']
 
-fruits.pop(0)     
-print(fruits)       # ['orange', 'mango'] 
+frutas.pop(0)     
+print(frutas)       # ['naranja', 'durazno'] 
 
 # del 
-fruits = ['banana', 'orange', 'mango', 'lemon']
-del fruits[0]     
-print(fruits)       # ['orange', 'mango', 'lemon']
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+del frutas[0]     
+print(frutas)       # ['naranja', 'durazno', 'limon']
 
-del fruits[1]     
-print(fruits)       # ['orange', 'lemon']
-del fruits
-print(fruits)       # This should give: NameError: name 'fruits' is not defined
+del frutas[1]     
+print(frutas)       # ['naranja', 'limon']
+del frutas
+print(frutas)       # This should give: NameError: name 'frutas' is not defined
 
 # clear
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.clear()     
-print(fruits)       # []
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+frutas.clear()     
+print(frutas)       # []
 
 # copying a lits
 
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits_copy = fruits.copy()     
-print(fruits_copy)       # ['banana', 'orange', 'mango', 'lemon']
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+frutas_copy = frutas.copy()     
+print(frutas_copy)       # ['platano', 'naranja', 'durazno', 'limon']
 
 # join
 positive_numbers = [1, 2, 3,4,5]
@@ -131,10 +131,9 @@ zero = [0]
 negative_numbers = [-5,-4,-3,-2,-1]
 integers = negative_numbers + zero + positive_numbers
 print(integers)
-fruits = ['banana', 'orange', 'mango', 'lemon']
-vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot'] 
-fruits_and_vegetables = fruits + vegetables
-print(fruits_and_vegetables )
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+vegetables = ['Tomates', 'Papas', 'Repollo','Cebolla', 'Zanahoria'] 
+frutas_and_vegetablelechefrutcarne vegmantequFinlandiarint(frutas_and_vegetables )
 
 # join with extend
 num1 = [0, 1, 2, 3]
@@ -148,36 +147,36 @@ zero = [0]
 negative_numbers.extend(zero)
 negative_numbers.extend(positive_numbers)
 print('Integers:', negative_numbers)
-fruits = ['banana', 'orange', 'mango', 'lemon']
-vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot'] 
-fruits.extend(vegetables)
-print('Fruits and vegetables:', fruits )
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+vegetables = ['Tomates', 'Papas', 'Repollo','Cebolla', 'Zanahoria'] 
+frutas.extend(vegetaleche)
+prcarne'frumanteFinlandia vegetables:', frutas )
 
 # count
-fruits = ['banana', 'orange', 'mango', 'lemon']
-print(fruits.count('orange'))   # 1
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+print(frutas.count('naranja'))   # 1
 ages = [22, 19, 24, 25, 26, 24, 25, 24]
 print(ages.count(24))           # 3
 
 # index
-fruits = ['banana', 'orange', 'mango', 'lemon']
-print(fruits.index('orange'))   # 1
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+print(frutas.index('naranja'))   # 1
 ages = [22, 19, 24, 25, 26, 24, 25, 24]
 print(ages.index(24)) 
 # Reverse
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.reverse()
-print(fruits)  
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+frutas.reverse()
+print(frutas)  
 ages = [22, 19, 24, 25, 26, 24, 25, 24]
 ages.reverse()
 print(ages) 
 
 # sort
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.sort()
-print(fruits) 
-fruits.sort(reverse=True)
-print(fruits)
+frutas = ['platano', 'naranja', 'durazno', 'limon']
+frutas.sort()
+print(frutas) 
+frutas.sort(reverse=True)
+print(frutas)
 ages = [22, 19, 24, 25, 26, 24, 25, 24]
 ages.sort()
 print(ages) 
