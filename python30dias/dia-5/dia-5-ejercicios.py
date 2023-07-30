e1a01b7162aa72e3dc25e917f65ac9f37afe9e83
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-07-30 09:17:13 -0400

    python desafio 30 dias dia 5

---
+++ python30dias/dia-5/dia-5-ejercicios.py
@@ -0,0 +1,91 @@
+# Declara una lista vacía.
+
+lista = []
+print(f"lista vacía: {lista} \n")
+
+
+# Declara una lista con más de 5 elementos.
+
+lista = [1,2,1,3,44,22,78]
+print(f"lista lista: {lista}\n")
+
+# Encuentra la longitud de tu lista.
+print(f"la longitud de la lista lista es: {len(lista)}\n")
+
+
+# Obtén el primer elemento, el elemento del medio y el último elemento de la lista.
+print(f"primer elemento: {lista[0]}")
+print(f"elemento del medio: {lista[len(lista)//2]}")
+print(f"ultimo elemento: {lista[-1]}\n")
+
+# Declara una lista llamada "mixed_data_types" y agrega tu (nombre, edad, altura, estado civil, dirección).
+mixed_data_types = ["Rodolfo", 51, 175, "casado", "siempre viva 123"]
+print(f"lista mixed_data_types: {mixed_data_types}\n") 
+
+# Declara una variable de lista llamada "it_companies" y asigna los valores iniciales: Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon.
+it_companies = ["Facebook", "Google", "Microsoft", "apple", "IBM", "Oracle", "Amazon" ]
+
+# Imprime la lista usando print().
+print(f"it_companies: {it_companies}\n") 
+
+# Imprime el número de compañías en la lista.
+print(f"numero de compañias: {len(it_companies)}\n")
+
+# Imprime la primera, la del medio y la última compañía.
+primer = it_companies[0]
+medio = it_companies[len(it_companies)//2]
+ultimo = it_companies[-1]
+
+print(f"primer: {primer}")
+print(f"medio: {medio}")
+print(f"ultimo: {ultimo}\n")
+
+# Imprime la lista después de modificar una de las compañías.
+it_companies[3] = 'TikTok'
+print('cambio Apple por tiktok')
+print(f"it_companies: {it_companies}\n") 
+
+# Agrega una compañía de TI a "it_companies".
+it_companies.append('Borland')
+print('agrego Borland')
+print(f"it_companies: {it_companies}\n") 
+
+# Inserta una compañía de TI en el medio de la lista de compañías.
+medio = len(it_companies) // 2
+it_companies.insert(medio, 'AliBaba')
+print(f"it_companies: {it_companies}\n") 
+
+"""
+Cambia uno de los nombres de las compañías de "it_companies" a mayúsculas (¡excluye IBM!).
+
+Combina las compañías de "it_companies" con un string "#; ".
+
+Comprueba si una cierta compañía existe en la lista "it_companies".
+
+Ordena la lista usando el método sort().
+
+Invierte la lista en orden descendente usando el método reverse().
+
+Extrae las primeras 3 compañías de la lista.
+
+Extrae las últimas 3 compañías de la lista.
+
+Extrae la(s) compañía(s) de TI del medio de la lista.
+
+Elimina la primera compañía de TI de la lista.
+
+Elimina la(s) compañía(s) de TI del medio de la lista.
+
+Elimina la última compañía de TI de la lista.
+
+Elimina todas las compañías de TI de la lista.
+
+Destruye la lista de compañías de TI.
+
+Combina las siguientes listas:
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node', 'Express', 'MongoDB']
+
+Después de combinar las listas del punto 26, copia la lista combinada y asígnala a una variable llamada "full_stack". Luego, inserta Python y SQL después de Redux.
+"""
