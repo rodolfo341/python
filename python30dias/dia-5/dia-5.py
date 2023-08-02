a7ee3e5d51d3b6887e009233d446ad7389868c6d
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-08-01 21:17:34 -0400

    python desafio 30 dias dia 5

--- python30dias/dia-5/dia-5.py
+++ python30dias/dia-5/dia-5.py
@@ -1,14 +1,14 @@
 import msvcrt
 def enter():
-    print("\n\tPresiona cualquier tecla para continuar... \n")
+    print("\n\t\t* * * * *  Presiona cualquier tecla para continuar  * * * * *\n")
     msvcrt.getch()
 
-enter()
-print("Esto es una lista vacía, no hay elementos en la lista.")
+print("\nEsto es una lista vacía, no hay elementos en la lista.")
+print("\n'lista_vacia = list()'")
 lista_vacia = list() # "Esto es una lista vacía, no hay elementos en la lista."
-print(len(lista_vacia)) # 0
+print(f"\nLongitud de la lista vacia\n'len(lista_vacia)' : {len(lista_vacia)}") # 0
 
-
+enter()
 frutas = ['platano', 'naranja', 'durazno', 'limon']                     # lista de frutas
 vegetales = ['Tomates', 'Papas', 'Repollo','Cebolla', 'Zanahoria']      # lista de vegetales
 productos_animales = ['leche', 'carne', 'mantequilla', 'yoghurt']             # lista de productos animales
@@ -16,7 +16,6 @@ tecnoligias_web = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] # lis
 paises = ['Finlandia', 'Estonia', 'Dinamarca', 'Suiza', 'Noruega']
 
 # Print de listas y longitud
-enter()
 print("Print de listas y longitud")
 
 print('frutas:', frutas)
@@ -35,16 +34,19 @@ print("Modificando lista\n")
 
 frutas = ['platano', 'naranja', 'durazno', 'limon'] 
 primera_fruta = frutas[0] # Accedemos al primer elemento usaando index
-print(primera_fruta)      # platano
+print(f"Accedemos al primer elemento usaando index: {primera_fruta}")      # platano
 segunda_fruta = frutas[1]
 print(segunda_fruta)     # naranja
 ultima_fruta = frutas[3]
 print(ultima_fruta) # limon
+
 # Ultimo indice
+print("Ultimo indice")
 ultimo_indice = len(frutas) - 1
 ultima_fruta = frutas[ultimo_indice]
 
 # Accediendo a los items
+print("Accediendo a los items")
 frutas = ['platano', 'naranja', 'durazno', 'limon'] 
 ultima_fruta = frutas[-1]
 penultima = frutas[-2]
@@ -52,16 +54,21 @@ print(ultima_fruta)       # limon
 print(penultima)      # durazno
 
 # Slicing items
+print("Slicing items")
 frutas = ['platano', 'naranja', 'durazno', 'limon'] 
 todas_las_frutas = frutas[0:4] # "Devuelve todas las frutas"
+
 # "Esto también da el mismo resultado que el anterior."
+print("Esto también da el mismo resultado que el anterior.")
 todas_las_frutas = frutas[0:] # "Si no especificamos dónde detenerse, toma todo el resto."
 naranja_y_durazno = frutas[1:3] # "No incluye el índice final."
 naranja_durazno_limon = frutas[1:]
 
 frutas = ['platano', 'naranja', 'durazno', 'limon'] 
+print("Devuelve todas las frutas")
 todas_las_frutas = frutas[-4:] # "Devuelve todas las frutas"
 # "Esto también da el mismo resultado que el anterior."
+print("Esto también da el mismo resultado que el anterior.")
 naranja_y_durazno = frutas[-3:-1] # "No incluye el índice final."
 naranja_durazno_limon = frutas[-3:]
 
@@ -76,6 +83,7 @@ frutas[ultimo_indice] = 'lime'
 print(frutas)        #  ['palta', 'manzana', 'durazno', 'lime']
 
 # checking items
+print("checking items")
 frutas = ['platano', 'naranja', 'durazno', 'limon']
 existe = 'platano' in frutas
 print(existe)  # True
@@ -83,6 +91,7 @@ existe = 'lime' in frutas
 print(existe)  # False
 
 # Append
+print("Append")
 frutas = ['platano', 'naranja', 'durazno', 'limon']
 frutas.append('manzana')
 print(frutas)           # ['platano', 'naranja', 'durazno', 'limon', 'manzana']
@@ -90,6 +99,7 @@ frutas.append('lime')   # ['platano', 'naranja', 'durazno', 'limon', 'manzana', 
 print(frutas)
 
 # insert
+print("insert")
 frutas = ['platano', 'naranja', 'durazno', 'limon']
 frutas.insert(2, 'manzana') # insert manzana between naranja and durazno
 print(frutas)           # ['platano', 'naranja', 'manzana', 'durazno', 'limon']
@@ -97,6 +107,7 @@ frutas.insert(3, 'lime')   # ['platano', 'naranja', 'manzana', 'durazno', 'lime'
 print(frutas)
 
 # remove
+print("remove")
 frutas = ['platano', 'naranja', 'durazno', 'limon']
 frutas.remove('platano')
 print(frutas)  # ['naranja', 'durazno', 'limon']
@@ -104,6 +115,7 @@ frutas.remove('limon')
 print(frutas)  # ['naranja', 'durazno']
 
 # pop
+print("pop")
 frutas = ['platano', 'naranja', 'durazno', 'limon']
 frutas.pop()     
 print(frutas)       # ['platano', 'naranja', 'durazno']
@@ -111,7 +123,8 @@ print(frutas)       # ['platano', 'naranja', 'durazno']
 frutas.pop(0)     
 print(frutas)       # ['naranja', 'durazno'] 
 
-# del 
+# del
+print("del")
 frutas = ['platano', 'naranja', 'durazno', 'limon']
 del frutas[0]     
 print(frutas)       # ['naranja', 'durazno', 'limon']
@@ -122,17 +135,19 @@ print(frutas)       # ['naranja', 'limon']
 print(frutas)       # "Esto debería dar como resultado: NameError: el nombre 'frutas' no está definido."
 
 # clear
+print("clear")
 frutas = ['platano', 'naranja', 'durazno', 'limon']
 frutas.clear()     
 print(frutas)       # []
 
 # copying a lits
-
+print("copying a lits")
 frutas = ['platano', 'naranja', 'durazno', 'limon']
 copiar_fruta = frutas.copy()     
 print(copiar_fruta)       # ['platano', 'naranja', 'durazno', 'limon']
 
 # join
+print("join")
 numeros_positivos = [1, 2, 3,4,5]
 cero = [0]
 numeros_negativos = [-5,-4,-3,-2,-1]
@@ -144,6 +159,7 @@ frutas_y_vegetales = frutas + vegetales
 print(frutas_y_vegetales)
 
 # join con extend
+print("join con extend")
 num1 = [0, 1, 2, 3]
 num2 = [4, 5,6]
 num1.extend(num2)
@@ -166,11 +182,14 @@ edades = [22, 19, 24, 25, 26, 24, 25, 24]
 print(edades.count(24))           # 3
 
 # index
+print("index")
 frutas = ['platano', 'naranja', 'durazno', 'limon']
 print(frutas.index('naranja'))   # 1
 edades = [22, 19, 24, 25, 26, 24, 25, 24]
 print(edades.index(24)) 
+
 # Reverse
+print("Reverse")
 frutas = ['platano', 'naranja', 'durazno', 'limon']
 frutas.reverse()
 print(frutas)  
@@ -179,6 +198,7 @@ edades.reverse()
 print(edades) 
 
 # sort
+print("sort")
 frutas = ['platano', 'naranja', 'durazno', 'limon']
 frutas.sort()
 print(frutas) 
