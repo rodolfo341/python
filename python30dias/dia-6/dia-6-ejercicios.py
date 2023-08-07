cbf27d57a688dc1ee70b5bee9392c280645f4cf3
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-08-07 19:56:25 -0400

    python 30 dias dia 6

---
+++ python30dias/dia-6/dia-6-ejercicios.py
@@ -0,0 +1,18 @@
+import msvcrt
+def enter():
+    print("\n\t\t* * * * *  Presiona cualquier tecla para continuar  * * * * *\n")
+    msvcrt.getch()
+    
+# Ejercicios: Nivel 1
+#    Crear una tupla vacía
+#    Crea una tupla que contenga los nombres de tus hermanas y tus hermanos (los hermanos imaginarios están bien)
+#    Unir tuplas de hermanos y hermanas y asignarlas a hermanos
+#    ¿Cuántos hermanos tiene usted?
+#    Modifique la tupla de hermanos y agregue el nombre de su padre y madre y asígnelo a family_members
+
+# Ejercicios: Nivel 1
+#    Crear una tupla vacía
+#    Crea una tupla que contenga los nombres de tus hermanas y tus hermanos (los hermanos imaginarios están bien)
+#    Unir tuplas de hermanos y hermanas y asignarlas a hermanos
+#    ¿Cuántos hermanos tiene usted?
+#    Modifique la tupla de hermanos y agregue el nombre de su padre y madre y asígnelo a family_members
