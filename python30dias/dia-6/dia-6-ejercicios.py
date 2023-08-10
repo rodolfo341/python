c8adccb0eb3a570afb6789fc5362ed7742cf2f15
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-08-09 20:14:41 -0400

    python 30 dias dia 6

--- python30dias/dia-6/dia-6-ejercicios.py
+++ python30dias/dia-6/dia-6-ejercicios.py
@@ -12,16 +12,23 @@ def enter():
 
 # Ejercicios: Nivel 1
 #    Crear una tupla vacía
+"""
 tuplaVacia = ()
 print(f"tupla vacia: {tuplaVacia}")
 print(f"cantidad de veces que esta el elemento: {tuplaVacia.count(2)}")
 enter()
+"""
 
 #    Crea una tupla que contenga los nombres de tus hermanas y tus hermanos (los hermanos imaginarios están bien)
-tuplaHermanos = ("Papa", "Mama", "Diere", "Gonky", "Manchas")
+tuplaHermanos = ("Ana", "Maria", "Sofia", "Laura")
+tuplaHermanas = ("Juan", "Carlos", "Andres", "Alejandro")
 print(f"tupla hermanos: {tuplaHermanos}")
+print(f"tupla hermanas: {tuplaHermanas}")
 enter()
 
 #    Unir tuplas de hermanos y hermanas y asignarlas a hermanos
+tuplaUnion = tuplaHermanos + tuplaHermanas
+print(f"tupla union: {tuplaUnion}")
+
 #    ¿Cuántos hermanos tiene usted?
 #    Modifique la tupla de hermanos y agregue el nombre de su padre y madre y asígnelo a family_members
